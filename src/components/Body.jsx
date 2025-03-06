6bb3ca7f02bf6e95d3150b7c6cb12869c0a205f7
Anil Yadav <anilyadav8149838058@gmail.com>
2025-03-06 16:46:59 +0530

    prevent calling APi again n again

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const userData = useSelector((store) => store.user) // this prevent from requesting api again and again for just changing page.
+    // prevent profile after refresh
+    const fetchUser = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/profile/view", {
+                withCredentials: true,
+            });
+            dispatch(addUser(res.data));
+
+        } catch (err) {
+            if (err.status === 401) {
+                navigate("/login");
+            }
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        if (!userData) {
+            fetchUser();
+        }
+    }, [])
     return (
         <div>
             <Navbar />
